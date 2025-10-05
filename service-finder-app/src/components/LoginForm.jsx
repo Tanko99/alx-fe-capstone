2da8032dc9d2aec04d0674c60782e430da9d3a5f
Tanko Fabrice Ngala <tankofabrice4@gmail.com>
2025-10-05 19:08:34 +0100

    feat:login form with styling

--- service-finder-app/src/components/LoginForm.jsx
+++ service-finder-app/src/components/LoginForm.jsx
@@ -1,73 +1,69 @@
-import { useState } from "react";
-import { useNavigate } from "react-router-dom"; 
 import useServiceStore from "./serviceStore";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const LoginForm = () => {
-  const [email, setEmail] = useState("");
-  const [password, setPassword] = useState("");
-  
-  const loginUser = useServiceStore((state) => state.loginUser);
-  const error = useServiceStore((state) => state.error);
-  const navigate = useNavigate();
+ function LoginForm () {
+  const loginUser = useServiceStore(State => State.loginUser);
+  const error = useServiceStore(state => state.error);
+  const [formData, setFormData] = useState({
+    email: '',
+    password: '',
+  });
+  const handleChange = (e) => {
+    setFormData({
+      ...formData, [e.target.name]: e.target.value
+    });
+    set({eror: ""});
+  };
 
-  const handleSubmit = (event) => {
-    event.preventDefault();
+  const handleSubmit = (e) => {
+     e.preventDefault();
 
-    const success = loginUser(email, password);
-
-    if (success) {
-      navigate("/home");  // go to homepage
-    }
-
-    setEmail("");
-    setPassword("");
+     const success = loginUser(email, password);
+     if(success){
+      Navigate("/home");
+      setFormData({
+        email: '',
+        password: '',
+      });
+     }
   };
 
   return (
-    <div className="flex flex-col min-h-screen p-4 sm:p-8 justify-center bg-black">
-      <h1 className="text-gray-100 text-center text-3xl sm:text-xl md:text-3xl font-bold mt-10">
-        Sign In
-      </h1>
-
-      <form onSubmit={handleSubmit} className="items-center justify-center shadow-md 
-      gap-y-4 flex flex-col">
-        <label className="text-gray-100 text-lg mb-1 font-medium" htmlFor="email">
-          Email
-        </label>
-        <input
-          className="w-full text-center text-gray-100 focus:border-purple-500 focus:ring-2 focus:ring-purple-600 sm:w-64 text-sm md:text-lg px-4 py-2 bg-black rounded-md shadow-lg outline-none"
-          id="email"
-          type="email"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-          placeholder="Enter your email address"
-          required
+    <div className="min-h-screen sm:px-8 px-4 text-white  bg-black">
+      <h1 className="md:text-5xl sm:text-3xl text-4xl text-center  font-bold">Welcome!</h1>
+      <form onSubmit={handleSubmit} className="flex  flex-col gap-y-4 w-full mx-auto items-center">
+        <h2 className="md:text-4xl sm:text-2xl text-lg text-center font-bold  mt-4 mb-6">Sign In</h2>
+        <label className="font-edium"  htmlFor="email">Email</label>
+        <input className="md:w-76 sm:w-64 focus:border-green-700 focus:ring-2 focus:ring-green-700 focus:outline-none
+        px-4 py-2 mb-2 rounded-lg shadow-lg"
+        id="email"
+        type="email"
+        value={formData.email}
+        onChange={handleChange}
+        placeholder="enter your email:"
+        required
         />
 
-        <label className="text-gray-100 text-lg mb-1" htmlFor="password">
-          Password
-        </label>
-        <input
-          className="w-full text-center text-gray-100 bg-black focus:border-purple-500 rounded-md focus:ring-2 focus:ring-purple-600 sm:w-64 text-sm md:text-lg mb-6 px-4 py-2"
-          id="password"
-          type="password"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          placeholder="Enter your password"
-          required
+        <label htmlFor="password" className="font-medium" >Password</label>
+        <input className="md:w-96 sm:w-64 focus:border-green-700 focus:ring-2 focus:ring-green-700 focus:outline-none
+        px-4 py-2 mb-2 rounded-lg shadow-lg"
+        id="password"
+        type="password"
+        value={formData.password}
+        onChange={handleChange}
+        placeholder="enter your password:"
+        required
         />
 
-        <button
-          className="py-2 px-6 text-gray-100 bg-purple-600 font-bold rounded-md hover:bg-purple-900"
-          type="submit"
-        >
-          Sign In
-        </button>
+        <button className="px-6 py-2 rounded-lg shadow-lg cursor-pointer bg-green-700" type="submit" > Log In</button>
       </form>
-
-      {error && <p className="text-red-600 text-base"> {error} </p>}
+    <p className="text-center md:text-xl sm:text-lg mt-2 text-base"> Don't have an account? {" "} 
+      <Link className="text-green-600 hover:underline" to="/register">Sign Up here</Link>
+    </p>
     </div>
   );
-};
+ }
 
-export default LoginForm;
+ export default LoginForm;
