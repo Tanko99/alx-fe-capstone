ea139de26d155f59afaee386c9b5d74dbf9fea46
Tanko Ngala <tankofabrice4@gmail.com>
2025-10-18 22:22:00 -0700

    Navbar adjusted and styled to add responsiveness

--- service-finder-app/src/components/Navbar.jsx
+++ service-finder-app/src/components/Navbar.jsx
@@ -1,44 +1,19 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-const Navbar = () => {
-  const location = useLocation(); // to highlight active link
-
+function Navbar(){
   return (
-    <nav className="flex justify-center gap-x-6 bg-purple-600 text-gray-100 py-4 shadow-md">
-      <Link
-        to="/home"
-        className={`transition-colors duration-300 hover:text-yellow-300 ${
-          location.pathname === "/home" ? "font-semibold underline" : ""
-        }`}
-      >
-        Home
-      </Link>
-      <Link
-        to="/services"
-        className={`transition-colors duration-300 hover:text-yellow-300 ${
-          location.pathname === "/services" ? "font-semibold underline" : ""
-        }`}
-      >
-        Services
-      </Link>
-      <Link
-        to="/"
-        className={`transition-colors duration-300 hover:text-yellow-300 ${
-          location.pathname === "/" ? "font-semibold underline" : ""
-        }`}
-      >
-        Login/Register
-      </Link>
-      <Link
-        to="/profile"
-        className={`transition-colors duration-300 hover:text-yellow-300 ${
-          location.pathname === "/profile" ? "font-semibold underline" : ""
-        }`}
-      >
-        Profile
-      </Link>
+    <nav className="bg-green-900 py-8 px-10">
+      <div className="flex flex-col md:flex-row justify-between gap-6">
+        <h1 className="text-xl font-bold text-left">ServiceFinder</h1>
+        <div className="flex flex-row gap-6">
+          <Link className="text-lg font-bold px-2 py-1 rounded-lg hover:bg-blue-700" to="/home">Home</Link>
+          <Link  className="text-lg font-bold px-2 py-1 rounded-lg hover:bg-blue-700" to="/services">Services</Link>
+          <Link  className="text-lg font-bold px-2 py-1 rounded-lg hover:bg-blue-700" to="/">Login/Register</Link>
+          <Link  className="text-lg font-bold px-2 py-1 rounded-lg hover:bg-blue-700" to="/profile">Profile</Link>
+        </div>
+      </div>
     </nav>
   );
-};
+}
 
 export default Navbar;
